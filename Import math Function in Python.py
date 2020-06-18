224dc05ecb6b79e9053d247843f7e8de75c11d25
Sanmesh Sabale <53466162+sanmeshsabale@users.noreply.github.com>
2020-06-18 10:05:36 +0530

    Math Operation using math Function

---
+++ Import math Function in Python.py
@@ -0,0 +1,52 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 24 2018, 00:16:47) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> x=math.sqrt(22)
+>>> print(x)
+4.69041575982343
+>>> x=math.floor(55)
+>>> print(x)
+55
+>>> x=math.ceil(4)
+>>> print(x)
+4
+>>> x=math.copysign(10,2)
+>>> print(x)
+10.0
+>>> x=math.factorial(5)
+>>> print(x)
+120
+>>> x=math.fmod(22,3)
+>>> print(x)
+1.0
+>>> x=math.isnan(55)
+>>> print(x)
+False
+>>> x=math.isnan(20)
+>>> print(x)
+False
+>>> x=math.exp(58)
+>>> print(x)
+1.545538935590104e+25
+>>> x=math.expm1(2)
+>>> print(x)
+6.38905609893065
+>>> x=math.log10(18)
+>>> print(x)
+1.255272505103306
+>>> x=math.tan(58)
+>>> print(x)
+8.330856852490458
+>>> x=math.sinh(11)
+>>> print(x)
+29937.07084924806
+>>> x=math.gamma(2)
+>>> print(x)
+1.0
+>>> x=math.pi
+>>> print(x)
+3.141592653589793
+>>> x=math.e
+>>> print(x)
+2.718281828459045
+>>> 
