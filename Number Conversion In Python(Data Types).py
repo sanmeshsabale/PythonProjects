ed762d9b9a94e9e5bdb78c900f151f0b67dbe065
Sanmesh Sabale <53466162+sanmeshsabale@users.noreply.github.com>
2020-06-18 10:08:08 +0530

    Number Conversion in Python

---
+++ Number Conversion In Python(Data Types).py
@@ -0,0 +1,11 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 24 2018, 00:16:47) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> bin(10)
+'0b1010'
+>>> hex(27)
+'0x1b'
+>>> oct(59)
+'0o73'
+>>> 0xf
+15
+>>> 
